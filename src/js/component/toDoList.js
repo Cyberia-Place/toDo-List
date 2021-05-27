5a8521d7d6285550cc9eeecee06911b25a5d5bc5
Cyberia-Place <eliasberto2@gmail.com>
2021-05-27 22:38:20 +0000

    avanzando

--- src/js/component/toDoList.js
+++ src/js/component/toDoList.js
@@ -3,14 +3,21 @@ import React, { useState } from "react";
 export const ToDoList = () => {
 	const [todoList, setTodoList] = useState([]);
 	const [todo, setTodo] = useState("");
+	const [visibility, setVisibility] = useState("");
 
 	const userInterface = todoList.map((element, index) => {
 		return (
-			<div key={index} className="input-style px-5 py-2">
+			<div
+				key={index}
+				className="input-style px-5 py-2"
+				onMouseEnter={() => setVisibility(index)}
+				onMouseLeave={() => setVisibility("")}>
 				{todoList[index]}
 				<button
 					type="button"
-					className="input-button-style bg-white"
+					className={`input-button-style bg-white ${
+						visibility === index ? "" : "d-none"
+					}`}
 					onClick={() => deleteToDo(index)}>
 					X
 				</button>
