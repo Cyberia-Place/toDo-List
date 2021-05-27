ae77c7edec119438d4bb74adf2bc4df173dd6202
Cyberia-Place <eliasberto2@gmail.com>
2021-05-27 00:40:33 +0000

    avanzando

---
+++ src/js/component/toDoList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const ToDoList = () => {
+	return (
+		<div className="table bg-light">
+			<h1>Lalaland</h1>
+			<input
+				type="text"
+				className="form-control"
+				placeholder="Username"
+				aria-label="Username"
+				aria-describedby="basic-addon1"
+			/>
+		</div>
+	);
+};
